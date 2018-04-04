3584b3a3d5d1e9815026f73dac236672276d3276
Maria Bartual <marialopezbartual@macbook-pro-de-maria.local>
2018-04-04 19:36:43 +0200

    Tab bar con navigator OK

---
+++ iCinema/TbController.swift
@@ -0,0 +1,35 @@
+//
+//  TbControllerViewController.swift
+//  iCinema
+//
+//  Created by Maria López Bartual on 4/4/18.
+//  Copyright © 2018 ls31360. All rights reserved.
+//
+
+import UIKit
+
+class TbController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
